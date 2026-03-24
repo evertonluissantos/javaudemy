4359bb053daa7c981de364429e18ed46e5d32777
Everton Luis <evertonluis.pro@outlook.com>
2026-03-23 23:50:11 -0300

    feat: adiciona estrutura condicional para saudação baseada no horário

---
+++ src/EstruturaCondicional/IfElse.java
@@ -0,0 +1,28 @@
+package EstruturaCondicional;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int hora;
+
+        System.out.print("Que horas são? ");
+        hora = sc.nextInt();
+
+        if (hora < 12) {
+            System.out.println("Bom Dia");
+        }
+        else {
+            if (hora >= 12 && hora <= 18) {
+                System.out.println("Boa Tarde");
+            }
+            else {
+                System.out.println("Boa Noite");
+            }
+        }
+
+        sc.close();
+    }
+}
