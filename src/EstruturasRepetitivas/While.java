96f744c0974f098ccec83891a3543fa74bc18963
Everton Luis <evertonluis.pro@outlook.com>
2026-04-08 23:31:53 -0300

    git commit -m "feat: adiciona exercício com estrutura while para soma de valores até entrada 0"

---
+++ src/EstruturasRepetitivas/While.java
@@ -0,0 +1,25 @@
+package EstruturasRepetitivas;
+
+import java.util.Scanner;
+
+public class While {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um valor: ");
+        int x = sc.nextInt();
+
+        int soma = 0;
+        while (x != 0) {
+            soma += x;
+            x = sc.nextInt();
+
+        }
+
+        System.out.println(soma);
+
+        sc.close();
+    }
+}
